d8e299f67d426d6c6835557af9d649a9eb47ff97
陈怡 <61664653@qq.com>
2019-03-21 13:26:57 +0800

    Commit2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  return "hello docker"
+  return "hello docker cloud"
 if __name__ == '__main__':
   app.run(debug=True,host='0.0.0.0', port=80)
 
